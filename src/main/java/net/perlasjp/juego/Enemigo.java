f3c13efc6c560e21cec12cb7db98c84e0d5f7d27
Fanny Iraheta <fanny.iraheta@icloud.com>
2025-07-23 10:18:39 -0600

    complete clase Enemigo

--- src/main/java/net/perlasjp/juego/Enemigo.java
+++ src/main/java/net/perlasjp/juego/Enemigo.java
@@ -2,4 +2,22 @@ package net.perlasjp.juego;
 
 public class Enemigo {
 
+   private String nombre;
+
+   private int salud;
+
+   private int fuerza;
+
+   public int atacar() {
+      return 0;
+   }
+
+   public void recibirDanio(int danio) {
+
+   }
+
+   public boolean estarVivo() {
+      return false;
+   }
+
 }
