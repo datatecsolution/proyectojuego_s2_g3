e8964fe6fbe0d5c451071c0d84ef2b90424ec67b
unknown <lopeztorresdulcemariana@gmail>
2025-07-23 10:25:45 -0600

    complete clase Jugador

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -2,7 +2,26 @@ package net.perlasjp.juego;
 
 public class Jugador {
 
+   private String nombre;
+   
+   private int salud;
+   
+   private int fuerza;
+ 
+    public int atacar() {
+        return fuerza;
+    }
+
+    public void recibirDanios(int danio) {
+        
+    }
+
+     public boolean estaVivo() {
+        return false;
+    }
+
     
 }
+
     
 
