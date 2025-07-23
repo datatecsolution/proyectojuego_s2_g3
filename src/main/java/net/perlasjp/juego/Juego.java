8c9505d00989d96eef2febe2c5f45fbfce30e79d
nathaly <nathalylainez1234@icloud.com>
2025-07-23 10:21:47 -0600

    complete clase Juego

--- src/main/java/net/perlasjp/juego/Juego.java
+++ src/main/java/net/perlasjp/juego/Juego.java
@@ -2,4 +2,11 @@ package net.perlasjp.juego;
 
 public class Juego {
 
+    private String Jugador;
+    private String Enemigo;
+    private String Combate;
+
+    public void iniciar() {
+
+    }
 }
