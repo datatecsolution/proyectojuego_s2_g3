d74ce466b9d8e12781b9e44a4c47538e625b73d8
Ruth Bardales <ruthbardales15@gmail.com>
2025-08-15 08:35:46 -0600

    complete funciones de clase Combate

--- src/main/java/net/perlasjp/juego/Combate.java
+++ src/main/java/net/perlasjp/juego/Combate.java
@@ -1,13 +1,48 @@
 package net.perlasjp.juego;
 
+import java.util.Scanner;
+
 public class Combate {
    
     private  Jugador jugador;
     private  Enemigo enemigo;
 
-    public void iniciar (Enemigo enemigo, Jugador jugador ){
+    public void iniciar (Jugador jugador, Enemigo enemigo) {
+           Scanner scanner = new Scanner (System.in);
+           //System.out.println("\niUn " + enemigo.getNombre() + "ha aparecido !" );
+
+
+       while (jugador.estaVivo() && enemigo.estarVivo()) {
+        //System.out.println("\nTu salud:" + jugador.getsalud() + " | salud del enemigo" + enemigo.getsalud());
+                    System.out.print("¿Atacar (a) o Huir (h)?");
+                    String opcion = scanner.nextLine();
+
+
+                    if (opcion.equalsIgnoreCase("a")) {
+                        enemigo.recibirDanio(jugador.atacar());
+                        if (enemigo.estarVivo()) {
+                            jugador.recibirDanios(enemigo.atacar());
+                        } else {
+                          //  System.out.println("¡Derrotaste al " + enemigo.getNombre() + "!");
+                        }
         
-    }
+                        }else if (opcion.equalsIgnoreCase("h")) {
+                            System.out.println("Has huido del combate.");
+                            return;
+                        }else{
+                            System.out.println("Opcion no valida.");
+                        }
+                        
+                    }
+          
+    
+           if (!jugador.estaVivo()) {
+               System.out.println("Has muerto en combate. Fin del juego.");
+               System.exit(0);
+               
+               
+            
+           } }
 
     
     
